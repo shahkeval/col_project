e33d8f1469a8c37c90684f728caf8a19513c0ac3
shahkeval <100589596+shahkeval@users.noreply.github.com>
2024-11-11 16:45:23 +0530

    Update salary.js

--- server/model/salary.js
+++ server/model/salary.js
@@ -1,28 +1,31 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    id:{
-        type : String,
-        required : true,
-        unique : true
+    id: {
+        type: String,
+        required: true,
+        unique: true
     },
-    s_eid:{
-        type : String,
-        required : true,
+    s_eid: {
+        type: String,
+        required: true,
     },
-    g_sal:{
-        type : Number,
-        required : true,
+    g_sal: {
+        type: Number,
+        required: true,
     },
-    t_sal:{
-        type : Number,
-        required : true,
+    t_sal: {
+        type: Number,
+        required: true,
     },
-    sal_amt:{
-        type : Number,
-        required : true,
+    sal_amt: {
+        type: Number,
+        required: true,
+    },
+    month: { // Added month field
+        type: String, // You can also use Number or Date depending on your needs
+        required: true,
     }
-    
 });
 
 const UserModelSalary = mongoose.model("Salary", UserSchema);
